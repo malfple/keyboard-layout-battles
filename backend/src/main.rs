6b22e8b31c80c9f589d3eb06b20b73bfc87d2c2e
malfple <noogai886@yahoo.com>
2024-02-01 19:55:14 +0800

    add backend scaffold (2)

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Router,
+};
+
+#[tokio::main]
+async fn main() {    
+    let app = Router::new().route("/", get(|| async {"Hello, World!"}));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
 }
---
+++ frontend/README.md
@@ -0,0 +1,1 @@
+# KLB Frontend
