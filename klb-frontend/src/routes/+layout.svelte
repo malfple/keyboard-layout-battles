f54edda78b7cb138f53bfd66c9494f69617c02fd
malfple <noogai886@yahoo.com>
2024-02-26 22:58:13 +0800

    try to add side bar

--- klb-frontend/src/routes/+layout.svelte
+++ klb-frontend/src/routes/+layout.svelte
@@ -1,56 +1,40 @@
 <script lang="ts">
 	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import { AppShell, AppRail, AppRailAnchor } from '@skeletonlabs/skeleton';
 </script>
 
-<!-- App Shell -->
 <AppShell>
-	<svelte:fragment slot="header">
-		<!-- App Bar -->
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
-			</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
-			</svelte:fragment>
-		</AppBar>
-	</svelte:fragment>
+	<AppRail slot="sidebarLeft" width="w-40" aspectRatio="aspect-[3/1]">
+		<AppRailAnchor href="/" title="Home">
+			<strong slot="lead" class="text-xl uppercase">KLB</strong>
+			<span>Keyboard Layout Battles</span>
+		</AppRailAnchor>
+		<AppRailAnchor
+			href="https://discord.gg/EXqV7W8MtY"
+			target="_blank"
+			rel="noreferrer"
+		>
+			Discord
+		</AppRailAnchor>
+		<AppRailAnchor
+			href="https://twitter.com/SkeletonUI"
+			target="_blank"
+			rel="noreferrer"
+		>
+			Twitter
+		</AppRailAnchor>
+		<AppRailAnchor
+			href="https://github.com/skeletonlabs/skeleton"
+			target="_blank"
+			rel="noreferrer"
+		>
+			GitHub
+		</AppRailAnchor>
+		<svelte:fragment slot="trail">
+			<AppRailAnchor aspectRatio="aspect-[5/1]" href="/login" title="Login">Sample Tail</AppRailAnchor>
+		</svelte:fragment>
+	</AppRail>
 	<!-- Page Route Content -->
-	
-	<svelte:fragment slot="sidebarLeft">
-		<!-- Insert the list: -->
-		<nav class="list-nav">
-			<ul>
-				<li><a href="/">Home</a></li>
-				<li><a href="/about">About</a></li>
-			</ul>
-		</nav>
-		<!-- --- -->
-	</svelte:fragment>
 
 	<slot />
 </AppShell>
