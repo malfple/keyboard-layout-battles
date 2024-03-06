9ad971bdab4bac3f73ec615954e03afec1879069
malfple <noogai886@yahoo.com>
2024-03-06 21:04:02 +0800

    use transform translate instead of top/left

--- klb-frontend/src/lib/keyboard/KeyMarker.svelte
+++ klb-frontend/src/lib/keyboard/KeyMarker.svelte
@@ -6,7 +6,7 @@
     export let ping: boolean;
 </script>
 
-<div class="absolute transition-all duration-200" style="top: {top}px; left: {left}px">
+<div class="absolute top-0 left-0 transition-all duration-200" style="transform: translate({left}px, {top}px);">
     <div class="absolute h-12 w-12 rounded-md border-2 border-error-500"></div>
     <div class="absolute h-12 w-12 rounded-md border-2 border-error-500 animate-ping" hidden={!ping}></div>
 </div>
