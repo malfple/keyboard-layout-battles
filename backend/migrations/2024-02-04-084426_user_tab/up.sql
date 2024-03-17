7b1393dc150228ece30e2a01e6a955e043e2c7e0
malfple <noogai886@yahoo.com>
2024-03-17 13:30:07 +0800

    add more layouts in migration

--- backend/migrations/2024-02-04-084426_user_tab/up.sql
+++ backend/migrations/2024-02-04-084426_user_tab/up.sql
@@ -29,14 +29,40 @@ CREATE TABLE layout_tab(
     UNIQUE INDEX unq_sequence_id(sequence_id)
 );
 
-INSERT INTO layout_tab(sequence_id, layout_data, rating, rating_comfort, time_created, time_modified, name)
+INSERT INTO layout_tab(sequence_id, layout_data, rating, rating_comfort, time_created, time_modified, name, description)
 VALUES
-    (1, 'qwertyuiopasdfghjkl;''zxcvbnm,./', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'QWERTY'),
-    (2, ''',.pyfgcrlaoeuidhtns-;qjkxbmwvz', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'Dvorak'),
-    (3, 'qwfpgjluy;arstdhneio''zxcvbkm,./', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'Colemak'),
-    (4, 'qwfpbjluy;arstgmneio''zxcdvkh,./', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'Colemak-DH'),
-    (5, 'qdrwbjfup;ashtgyneoi''zxmcvkl,./', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'Workman'),
-    (6, 'ypoujkdlcwinea,mhtsr''qz/.:bfgvx', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'MTGAP');
+    (1, 'qwertyuiopasdfghjkl;''zxcvbnm,./', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'QWERTY', 'Does this even need any explanation? - Christopher Latham Sholes 1870-ish'),
+    (2, ''',.pyfgcrlaoeuidhtns-;qjkxbmwvz', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'Dvorak', 'A popular alternative by August Dvorak 1936'),
+	(3, 'ql.p'';fudkarenbgsito/zw,hjvcymx', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'Arensito (Simplified)', 'Developed by HÃ¥kon Hallingstad in 2001'),
+    (4, 'qwfpgjluy;arstdhneio''zxcvbkm,./', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'Colemak', 'Another popular alternative by Shai Coleman 2006. Easy to learn'),
+    (5, 'qwfpbjluy;arstgmneio''zxcdvkh,./', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'Colemak-DH', 'A popular mod to Colemak aimed for ortho and col-stag keyboards. 2014'),
+    (6, 'qdrwbjfup;ashtgyneoi''zxmcvkl,./', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'Workman', 'Born out of the dissapointment with Colemak. By OJ Bucao 2010'),
+	(7, 'qwdfkjurl;asetgynioh''zxcvbpm,./', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'Norman', 'Only 4 key difference from QWERTY!'),
+	(8, 'qwdfkyuil;astrghneop''zxcvbjm,./', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'Minimak-12', 'Only 12 key difference from QWERTY!!!'),
+	(9, 'wlrbz;qudjshnt,.aeoi''fmvc/gpxky', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'Halmak', 'AI designed. Such technology!'),
+	(10, 'flhvzqwuoysrntkcdeai;x''bmjpg,./', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'Semimak', 'A keyboard layout made from genkey, an analyzer focusing on same-finger skipgrams'),
+	(11, 'flhvz''wuoysrntkcdeai;xjbmqpg,./', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'Semimak-JQ', 'Improved Semimak'),
+	(12, 'flhvz''wuoysrntkgdeai;xjbmqpc,./', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'Semimak-JQC', 'Upgraded Improved Semimak!'),
+	(13, 'trewqpoiuygfdsa;lkjh''bvcxz/.,mn', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'inqwerted', 'The best keyboard layout to have ever been created'),
+    (14, 'ypoujkdlcwinea,mhtsr''qz/.:bfgvx', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'MTGAP-30', 'The Keyboard Layout Project. This is the 30 key version'),
+	(15, 'qplcjxfou/rntsgyheia;zbmwvkd'',.', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'CTGAP', 'A series of keyboard layouts by Colby. This is the final version'),
+	(16, 'wgdfbqluoyrsthkjneai;xcmpvz,.''/', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'APTv3', 'Based on MTGAP and CTGAP'),
+	(17, 'wlypkzxou;crstbfneia''jvdgqmh/,.', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'Canary', 'AKL Discord Collab! Guess the number of creators!'),
+	(18, 'wlypbzfou''crstgmneia;qjvdkxh/,.', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'Canary-ortho', 'Canary but for Ortholinear keyboards'),
+	(19, 'flhdmvwou,srntkgyaei/xjbzqpc'';.', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'Whorf', 'Whorf!'),
+	(20, 'xdh.qbfoujrtna;gweis/lkm,''pczyv', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'RTNA', 'Experimental Layout to get lowest SFB% on colemak mods analyzer'),
+	(21, 'xldkvzwou;srtnfgyeia/qjmhbpc'',.', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'Sertain', 'low-redirect layout'),
+	(22, 'pldvxzkou;nrtsgyheia/qjmcwbf'',.', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'Nerts', 'Pretty much nrts-haei'),
+	(23, 'xldpvzkou;nrtsgyheia/qjmcwbf'',.', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'Nerps', 'Pretty much nrts-haei. Also beginner friendly!'),
+	(24, 'bldwz''foujnrtsgyhaei,qxmcvkp.-/', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'Graphite', 'Much liked nrts-haei'),
+	(25, 'bldcvjyou,nrtsgphaei/xqmwzkf'';.', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'Gallium', 'Much comfort nrts-haei'),
+	(26, 'bldgqjfou,nrtsvkhaei;zxmcwpy''/.', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'Maya', 'Much fast nrts-haei'),
+	(27, 'vmlcpxfoujstrdy.naei-zkqgwbh'';,', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'Sturdy', 'Get sturdy'),
+	(28, 'yclmkzfu,''isrtgpneao;qvwdjbh/.x', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'ISRT', 'Very straightforward name'),
+	(29, 'ylcmkzfu,''irstwbneao;qjgdvph".x', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'IRST', 'Is this ISRT''s sister?'),
+	(30, 'frdpvqjuoysntcb.heai/zxkgwml;'',', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'Recurva', 'Beats Semimak in stats, but will it also achieve victory in battle???'),
+	(31, 'qhou,glrfbjieaydtsnp;xk".''vmcwz', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'BEAKL-43', 'BEAKL Fam'),
+	(32, 'tkvumiajbrwx/fpdgq,s;h.''colnzye', 1500, 1500, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'Vitrimak', 'A popular and a very good layout (trust)');
 
 CREATE TABLE battle_tab(
     id VARCHAR(32) NOT NULL,
