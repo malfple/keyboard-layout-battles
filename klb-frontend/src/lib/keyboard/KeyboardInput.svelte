0050163f17a9685fc00efd8541aedaf5c5b0b36d
malfple <noogai886@yahoo.com>
2024-03-25 21:08:14 +0800

    add prevent default for battle page too

--- klb-frontend/src/lib/keyboard/KeyboardInput.svelte
+++ klb-frontend/src/lib/keyboard/KeyboardInput.svelte
@@ -1,7 +1,7 @@
 <script lang="ts" context="module">
     const REQUIRED_CHARS = "abcdefghijklmnopqrstuvwxyz'"
     export const ALLOWED_CHARS = "abcdefghijklmnopqrstuvwxyz.,<>;:\'\"/?()[]{}-";
-    const CHAR_PREVENT_DEFAULT = ".,<>;:\'\"/?()[]{}-";
+    export const CHAR_PREVENT_DEFAULT = ".,<>;:\'\"/?()[]{}-";
     const POS_TOP = [
         0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
         52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52,
